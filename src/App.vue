5e48d3f1d05a3577a00b5191229bf737a8b7d0ea
lorenzo <lor.abbadessa@gmail.com>
2023-11-30 17:55:50 +0100

    continua esercizio

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      search: ""
+    }
+  },
+  mounted() {
+    axios.get("https://api.openbrewerydb.org/v1/breweries?by_country=austria&per_page=10").then(result => {
+      console.log(result)
+    })
+  }
+}
+</script>
+
+
+<template>
+  <h1>BIRRERIE</h1>
+  <input type="text" placeholder="Cerca il tuo Bar"> <br> <br>
+  <button>INVIA</button>
+</template>
+
+
+<style>
+body {
+  display: flex;
+  justify-content: center;
+
+}
+</style>
